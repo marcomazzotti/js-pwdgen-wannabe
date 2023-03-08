d14e19770e700b82ffeba675df8db445cd399f18
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-03-08 09:48:19 +0100

    Correzioni

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 // userName
-const userName = prompt('Inserisci il tuo nome')
-console.log(userName, typeof userName)
+const userName = prompt('Inserisci il tuo nome');
+console.log(userName, typeof userName);
 
 // surname
-const surname = prompt('Inserisci il tuo cognome')
-console.log(surname, typeof surname)
+const surname = prompt('Inserisci il tuo cognome');
+console.log(surname, typeof surname);
 
 // color
-const color = prompt('Inserisci il tuo colore preferito')
-console.log(color, typeof color)
+const color = prompt('Inserisci il tuo colore preferito');
+console.log(color, typeof color);
+
+//number
+const number = 21;
 
 //test
-const message = `${userName}${surname}${color}21`
+const message = userName + surname + color + number
 console.log(message);
 
 //password
